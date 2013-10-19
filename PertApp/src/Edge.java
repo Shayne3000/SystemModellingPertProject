5d38dce67936381d603f5f2894c9e3b1f1f9c2b4
Shayne3000 <shaynijoshua@gmail.com>
2013-10-19 17:00:43 +0300

    System Modelling Project Files and then some....

---
+++ PertApp/src/Edge.java
@@ -0,0 +1,19 @@
+/**
+ * @(#) Edge.java
+ */
+
+public class Edge
+{
+	private PERT within;
+	
+	private Task Source;
+	
+	private Task Destination;
+	
+	public void IsinCriticalPath( )
+	{
+		
+	}
+	
+	
+}
---
+++ PertApp/src/PERT.java
@@ -0,0 +1,29 @@
+/**
+ * @(#) PERT.java
+ */
+
+public class PERT
+{
+	private Task contains;
+	
+	private Edge has;
+	
+	private String name;
+	
+	public void getTasks( )
+	{
+		
+	}
+	
+	public void getEdges( )
+	{
+		
+	}
+	
+	public void CalculateCriticalPath( )
+	{
+		
+	}
+	
+	
+}
